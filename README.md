8f98b55dea751d5e82b9c7a0d2a54ba4b4a223e2
Justin Huffman <justin.t.huffman@gmail.com>
2018-10-07 23:19:14 -0400

    updated song repo with songs/additional genre. updated display and started nesting lists. Made readme.md

---
+++ README.md
@@ -0,0 +1,2 @@
+#This is a list of top ten bangers by genre.
+#It pulls the list from a respository I created and uses Spring/MVC to display it on the web via HTML templates in the templates directory.
--- src/main/java/org/wecancodeit/songreviews/repositories/SongRepository.java
+++ src/main/java/org/wecancodeit/songreviews/repositories/SongRepository.java
@@ -12,10 +12,25 @@ public class SongRepository {
 	private Map<Long, Song> songs = new HashMap<>();
 
 	public SongRepository() {
-		add(new Song(1L, "A Milli", "Lil Wayne", "Tha Carter III", "amili.jpg", "Hip Hop Bangers", "March 11, 2008",
+		add(new Song(1L, "Backseat Freestyle", "Kendrick Lamar", "good kid, M.A.A.d. city", "goodkid.jpg", "Hip Hop",
+				"October 22, 2012", "GREATREVIEW"));
+		add(new Song(2L, "Close Your Eyes (And Count to Fuck)", "Run the Jewels", "Run the Jewels 2", "rtj.jpg",
+				"Hip Hop", "October 13, 2014", "GREATREVIEW"));
+		add(new Song(3L, "Tuf Tiddy", "Shredders", "Dangerous Jumps", "shredders.jpg", "Hip Hop", "November 03, 2017",
 				"GREATREVIEW"));
-		add(new Song(2L, "Backseat Freestyle", "Kendrick Lamar", "good kid, M.A.A.d. city", "goodkid.jpg",
-				"Hip Hop Bangers", "October 22, 2012", "GREATREVIEW"));
+		add(new Song(4L, "Big Beast (ft. Bun B, T.I., and Trouble)", "Killer Mike", "R.A.P. Music", "killermike.jpg",
+				"Hip Hop", "March 16, 2012", "GREATREVIEW"));
+		add(new Song(5L, "A Milli", "Lil Wayne", "Tha Carter III", "amili.jpg", "Hip Hop", "March 11, 2008",
+				"GREATREVIEW"));
+		add(new Song(6L, "Back in Black", "AC/DC", "Back in Black", "acdc.jpg", "Rock", "December 21, 1980",
+				"GREATREVIEW"));
+		add(new Song(7L, "Immigrant Song", "Led Zeppelin", "Led Zeppelin III", "ledzep.jpg", "Rock", "November 5, 1970",
+				"GREATREVIEW"));
+		add(new Song(8L, "Who Are you", "The Who", "Who Are You", "who.jpg", "Rock", "July 14, 1978", "GREATREVIEW"));
+		add(new Song(9L, "(I Can't Get No) Satisfaction", "The Rolling Stones", "Out of Our Heads", "stones.jpg",
+				"Rock", "May 12, 1965", "GREATREVIEW"));
+		add(new Song(10L, "Bohemian Rhapsody", "Queen", "A Night at the Opera", "queen.jpg", "Rock", "August 31, 1975",
+				"GREATREVIEW"));
 	}
 
 	public int size() {
