ee33c491df9b267659ab3fa304eae46ab9fdb9cd
Justin Huffman <justin.t.huffman@gmail.com>
2018-10-07 20:06:05 -0400

    first commit

---
+++ src/main/java/org/wecancodeit/songreviews/SongReviewsApplication.java
@@ -0,0 +1,12 @@
+package org.wecancodeit.songreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongReviewsApplication.class, args);
+	}
+}
